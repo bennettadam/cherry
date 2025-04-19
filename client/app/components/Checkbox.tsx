c784026ba07132e0c8655dcc145d732cddc5ff59
qmitchell-aa <139587172+qmitchell-aa@users.noreply.github.com>
2025-04-18 22:26:39 -0400

    Create Checkbox.tsx

---
+++ client/app/components/Checkbox.tsx
@@ -0,0 +1,14 @@
+import Checkmark from './Checkmark'
+import UnfilledCircle from './UnfilledCircle'
+
+interface CheckboxProps {
+  isSelected: boolean
+}
+
+export function Checkbox({ isSelected }: CheckboxProps) {
+  return (
+    isSelected 
+      ? <Checkmark className="h-5 w-5 fill-sky-600" /> 
+      : <UnfilledCircle className="h-5 w-5 fill-gray-400" />
+  )
+}
