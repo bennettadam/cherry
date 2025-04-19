7a54f34847993180f9e83a98d24b8bb3282d0a68
qmitchell-aa <139587172+qmitchell-aa@users.noreply.github.com>
2025-04-18 22:41:31 -0400

    fix spacing

--- client/app/components/Checkbox.tsx
+++ client/app/components/Checkbox.tsx
@@ -2,13 +2,13 @@ import Checkmark from './Checkmark'
 import UnfilledCircle from './UnfilledCircle'
 
 interface CheckboxProps {
-  isSelected: boolean
+	isSelected: boolean
 }
 
 export function Checkbox({ isSelected }: CheckboxProps) {
-  return (
-    isSelected 
-      ? <Checkmark className="h-5 w-5 fill-sky-600" /> 
-      : <UnfilledCircle className="h-5 w-5 fill-gray-400" />
-  )
+	return (
+		isSelected 
+			? <Checkmark className="h-5 w-5 fill-sky-600" /> 
+			: <UnfilledCircle className="h-5 w-5 fill-gray-400" />
+	)
 }
